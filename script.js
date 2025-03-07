291383f2f6d3478bdf77ddc562f6e4f390b6a216
varletint <intvarlet@gmail.com>
2025-03-07 04:59:00 +0100

    ccc

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const hamburgerIcon = document.querySelector(".hamburger-menu");
 const menuLinks = document.querySelector(".menu-links");
+const menu = 1;
 
 hamburgerIcon.addEventListener("click", () => {
   menuLinks.classList.toggle("active");
